b20bb943408fa8e91ff5a4090bdf4924b535de56
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-05 23:29:38 +0530

    Go Data Type
    String , Int , Float

---
+++ Go Variables/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myname string = "Suraj"
+	fmt.Println(myname)
+
+	var myage int = 25
+	fmt.Println("Suraj age ", myage)
+
+	var mySalary float64 = 28044.00
+	fmt.Println(mySalary)
+
+}
