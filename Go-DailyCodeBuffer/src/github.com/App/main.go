c6dc93b945e75e96c21877a0d1f15e3d5616f1bd
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-12 18:01:02 +0530

    Go variables

--- Go-DailyCodeBuffer/src/github.com/App/main.go
+++ Go-DailyCodeBuffer/src/github.com/App/main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+
+	var i int = 10
+	var j float64 = 20.5
+
+	fmt.Println("Value of i is : ", i)
+	fmt.Println("Value of j is : ", j)
+
+	fmt.Println("Address of i is : ", &i)
+	fmt.Println("Address of j is : ", &j)
+
+	fmt.Printf("%v,%T\n", i, i)
+	fmt.Printf("%v , %T\n", j, j)
+
 }
