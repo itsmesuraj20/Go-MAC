ff968ba163564a7b8e3e8d10e9909d7ee1a08f72
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-12 18:05:49 +0530

    Shadowing Variable and Type Conversing

--- Go-DailyCodeBuffer/src/github.com/App/main.go
+++ Go-DailyCodeBuffer/src/github.com/App/main.go
@@ -4,16 +4,26 @@ import "fmt"
 
 func main() {
 
-	var i int = 10
-	var j float64 = 20.5
+	// var i int = 10
+	// var j float64 = 20.5
 
-	fmt.Println("Value of i is : ", i)
-	fmt.Println("Value of j is : ", j)
+	//Type Conversion
+	var a = 10
+	var b float32 = float32(a)
 
-	fmt.Println("Address of i is : ", &i)
-	fmt.Println("Address of j is : ", &j)
+	fmt.Println("Value of a is : ", a)
+	fmt.Println("Value of b is : ", b)
 
-	fmt.Printf("%v,%T\n", i, i)
-	fmt.Printf("%v , %T\n", j, j)
+	fmt.Printf("%v,%T\n", a, a)
+	fmt.Printf("%v , %T\n", b, b)
+
+	//fmt.Println("Value of i is : ", i)
+	//fmt.Println("Value of j is : ", j)
+
+	//fmt.Println("Address of i is : ", &i)
+	//fmt.Println("Address of j is : ", &j)
+
+	//fmt.Printf("%v,%T\n", i, i)
+	//fmt.Printf("%v , %T\n", j, j)
 
 }
