558ec79c81e6d915bf2aae67f7517020f967593e
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-13 18:05:12 +0530

    Complex64, Complex128

--- Go-Primitive Types/main.go
+++ Go-Primitive Types/main.go
@@ -5,17 +5,26 @@ import (
 )
 
 func main(){
-	var i float32
-	var j float64
+	// var i float32
+	// var j float64
 
-	fmt.Println("Value of i is : ", i)
-	fmt.Println("Value of j is : ", j)
+	a := 1 + 2i
+	b := 1 + 2i
 
-	fmt.Println("Address of i is : ", &i)
-	fmt.Println("Address of j is : ", &j)
+	fmt.Printf("%v,%T\n", real(a), real(a))
+	fmt.Printf("%v,%T\n", imag(a), imag(a))
 
-	fmt.Printf("%v,%T\n", i, i)
-	fmt.Printf("%v , %T\n", j, j)
+	fmt.Printf("%v,%T\n", real(b), real(b))
+	fmt.Printf("%v,%T\n", imag(b), imag(b))
+
+	// fmt.Println("Value of i is : ", i)
+	// fmt.Println("Value of j is : ", j)
+
+	// fmt.Println("Address of i is : ", &i)
+	// fmt.Println("Address of j is : ", &j)
+
+	// fmt.Printf("%v,%T\n", i, i)
+	// fmt.Printf("%v , %T\n", j, j)
 
 	// fmt.Println(i+j)
 	// fmt.Println(i-j)
