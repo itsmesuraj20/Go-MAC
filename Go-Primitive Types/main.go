40edd52dba695b48005d3efea78c34c9d90f5c79
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-13 18:30:57 +0530

    String , Byte, Rune

--- Go-Primitive Types/main.go
+++ Go-Primitive Types/main.go
@@ -4,32 +4,53 @@ import (
 	"fmt"
 )
 
+// func PrimitiveData(){
+// 	// var i float32
+// 	// var j float64
+
+// 	// a := 1 + 2i
+// 	// b := 1 + 2i
+
+// 	// fmt.Printf("%v,%T\n", real(a), real(a))
+// 	// fmt.Printf("%v,%T\n", imag(a), imag(a))
+
+// 	// fmt.Printf("%v,%T\n", real(b), real(b))
+// 	// fmt.Printf("%v,%T\n", imag(b), imag(b))
+
+// 	// fmt.Println("Value of i is : ", i)
+// 	// fmt.Println("Value of j is : ", j)
+
+// 	// fmt.Println("Address of i is : ", &i)
+// 	// fmt.Println("Address of j is : ", &j)
+
+// 	// fmt.Printf("%v,%T\n", i, i)
+// 	// fmt.Printf("%v , %T\n", j, j)
+
+// 	// fmt.Println(i+j)
+// 	// fmt.Println(i-j)
+// 	// fmt.Println(i*j)
+// 	// fmt.Println(i/j)
+
+// }
+
 func main(){
-	// var i float32
-	// var j float64
-
-	a := 1 + 2i
-	b := 1 + 2i
-
-	fmt.Printf("%v,%T\n", real(a), real(a))
-	fmt.Printf("%v,%T\n", imag(a), imag(a))
-
-	fmt.Printf("%v,%T\n", real(b), real(b))
-	fmt.Printf("%v,%T\n", imag(b), imag(b))
-
-	// fmt.Println("Value of i is : ", i)
-	// fmt.Println("Value of j is : ", j)
-
-	// fmt.Println("Address of i is : ", &i)
-	// fmt.Println("Address of j is : ", &j)
-
-	// fmt.Printf("%v,%T\n", i, i)
-	// fmt.Printf("%v , %T\n", j, j)
-
-	// fmt.Println(i+j)
-	// fmt.Println(i-j)
-	// fmt.Println(i*j)
-	// fmt.Println(i/j)
+	fmt.Println("String")
 
 
+	var s string = "Hello World"
+	fmt.Println(s)
+
+	str := "Short Hello World"
+	fmt.Println(str)
+
+	fmt.Printf("%v,%T\n", s, s)
+	fmt.Printf("%v,%T\n", str, str)
+
+	var arr []byte = []byte(s)
+	fmt.Printf("%v,%T\n", arr, arr)
+	
+	var arr1 []byte = []byte(str)
+	fmt.Printf("%v,%T\n", arr1, arr1)
+
+	
 }
