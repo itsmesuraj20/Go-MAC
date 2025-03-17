38b378fe01c8c688bc23f32be03eeb9c39b6cd7a
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-17 19:52:39 +0530

    Array and Slices

--- Go-Array & Slices/main.go
+++ Go-Array & Slices/main.go
@@ -29,33 +29,36 @@ func main() {
 
 
 
-	// Slices
-	newArr := []int {15,30,45,60,75}
-	fmt.Println("newARR :",newArr)
+	// // Slices
+	// newArr := []int {15,30,45,60,75}
+	// fmt.Println("newARR :",newArr)
 
-	newArr2 := &newArr
-	fmt.Println("newARR2:", *newArr2)
+	// newArr2 := &newArr
+	// fmt.Println("newARR2:", *newArr2)
 
-	// Slice of a slice
-	newSlice1 := newArr[:] // Slice of the entire array
-	newSlice2 := newArr[1:3] // 1 is inclusive and 3 is exclusive
-	newSlice3 := newArr[:3] //	0 is inclusive and 3 is exclusive
-	newSlice4 := newArr[2:] // 2 is inclusive and till the end
-	newSlice5 := newArr[0:4] // 1 is inclusive and 4 is exclusive
-			//OR
-	newSlice5v2 := newArr[:len(newArr)-1] // 1 is inclusive and 5 is exclusive		
-	newSlice6 := newArr[1:] // 1 is inclusive and till the end
+	// // Slice of a slice
+	// newSlice1 := newArr[:] // Slice of the entire array
+	// newSlice2 := newArr[1:3] // 1 is inclusive and 3 is exclusive
+	// newSlice3 := newArr[:3] //	0 is inclusive and 3 is exclusive
+	// newSlice4 := newArr[2:] // 2 is inclusive and till the end
+	// newSlice5 := newArr[0:4] // 1 is inclusive and 4 is exclusive
+	// 		//OR
+	// newSlice5v2 := newArr[:len(newArr)-1] // 1 is inclusive and 5 is exclusive		
+	// newSlice6 := newArr[1:] // 1 is inclusive and till the end
 
-	fmt.Println()
-	fmt.Println("--------------------")
-	fmt.Println()
-	fmt.Println("newSlice1:",newSlice1)
-	fmt.Println("newSlice2:",newSlice2)
-	fmt.Println("newSlice3:",newSlice3)
-	fmt.Println("newSlice4:",newSlice4)
-	fmt.Println("newSlice5:",newSlice5)
-	fmt.Println("-----OR-----")
-	fmt.Println("newSlice5v2:",newSlice5v2)
-	fmt.Println("newSlice6:",newSlice6)
+	// fmt.Println()
+	// fmt.Println("--------------------")
+	// fmt.Println()
+	// fmt.Println("newSlice1:",newSlice1)
+	// fmt.Println("newSlice2:",newSlice2)
+	// fmt.Println("newSlice3:",newSlice3)
+	// fmt.Println("newSlice4:",newSlice4)
+	// fmt.Println("newSlice5:",newSlice5)
+	// fmt.Println("-----OR-----")
+	// fmt.Println("newSlice5v2:",newSlice5v2)
+	// fmt.Println("newSlice6:",newSlice6)
+
+
+	
 
 }
