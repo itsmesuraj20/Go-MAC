0b472b5f67e98961e79acafc257ec9723b46bc58
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2025-03-17 21:37:14 +0530

    Go-Maps

--- Go-Maps/main.go
+++ Go-Maps/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 func main() {
@@ -13,33 +12,42 @@ func main() {
 	shopping = map[string] int {
 		"Apple" : 10,
 		"Banana": 20,
+		"Suraj" : 30,
 		"Orange": 30,
 		"Kiwi"  : 40,
 	}
 
+	// fmt.Println(shopping)
+	// fmt.Println(len(shopping))
+	// fmt.Println(reflect.TypeOf(shopping))
+	
+	// fmt.Println(shopping["Apple"])
+	// fmt.Println(shopping["Banana"])
+	// fmt.Println(shopping["Orange"])
+	// fmt.Println(shopping["Kiwi"])
+
+	// // Adding a new key-value pair
+	// shopping["Mango"] = 50
+	// fmt.Println(shopping)
+
+
+	// fmt.Println(" ---------- ------------")
+	// //check if a key exists
+	
+	// _, ok := shopping["Apple"] // _ is a blank identifier , that will ignore the value of the key 
+	// fmt.Println(ok)
+	// 			//OR
+
+	// cart, ok := shopping["Loan"]
+	// fmt.Println(cart, ok)
+	
+	sc := shopping
 	fmt.Println(shopping)
-	fmt.Println(len(shopping))
-	fmt.Println(reflect.TypeOf(shopping))
+	fmt.Println(sc)
 	
-	fmt.Println(shopping["Apple"])
-	fmt.Println(shopping["Banana"])
-	fmt.Println(shopping["Orange"])
-	fmt.Println(shopping["Kiwi"])
-
-	// Adding a new key-value pair
-	shopping["Mango"] = 50
+	sc["Suraj"] = 99999999 //while copying do consider that it will change the original map as well , as it is a reference type 
+	
 	fmt.Println(shopping)
-
-
-	fmt.Println(" ---------- ------------")
-	//check if a key exists
-	
-	_, ok := shopping["Apple"] // _ is a blank identifier , that will ignore the value of the key 
-	fmt.Println(ok)
-				//OR
-
-	cart, ok := shopping["Loan"]
-	fmt.Println(cart, ok)
-	
+	fmt.Println(sc)
 	
 }
